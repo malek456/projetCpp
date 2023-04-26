3595512a280ef4e4f37bf9e63d7c6693c77d5f55
youssra <youssra.bendaoued@esprit.tn>
2023-04-26 10:27:07 +0200

    fisrt commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "vehicule.h"
+#include <QApplication>
+#include <QSqlTableModel>
+#include <QApplication>
+#include <QMessageBox>
+#include "connection.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+
+
+
+
+
+       Connection c;
+       bool test=c.createconnect();
+   MainWindow w;
+   vehicule v;
+       if(test)
+       {v.show();
+           QMessageBox::critical(nullptr, QObject::tr("database is open"),
+                       QObject::tr("connection successful.\n"
+                                   "Click Cancel to exit."), QMessageBox::Cancel);
+
+   }
+       else
+           QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                       QObject::tr("connection failed.\n"
+                                   "Click Cancel to exit."), QMessageBox::Cancel);
+
+    return a.exec();
+}
